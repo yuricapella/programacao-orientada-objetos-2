3c17bf812931af874528b09f75af276c411b4273
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-02-17 17:09:18 -0300

    adiciona exercicio 04 prático, refatora nome da pasta exercicios

---
+++ src/Aula_03/Solid/Exercicios_S_O/Exercicio04/Produtos/Movel.java
@@ -0,0 +1,7 @@
+package Aula_03.Solid.Exercicios_S_O.Exercicio04.Produtos;
+
+public class Movel extends Produto {
+    public Movel(String nome, int quantidade) {
+        super(nome, quantidade);
+    }
+}
---
+++ src/Aula_03/Solid/Exercicios_S_O/Exercicio04/Produtos/Produto.java
@@ -0,0 +1,35 @@
+package Aula_03.Solid.Exercicios_S_O.Exercicio04.Produtos;
+
+public abstract class Produto {
+    private long identificador;
+    private String nome;
+    private int quantidade;
+    private static long contador = 1;
+
+    public Produto(String nome, int quantidade) {
+        this.identificador = contador++;
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+
+    public long getIdentificador() {
+        return identificador;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Id=" + identificador + ", Nome='" + nome + "', Quantidade=" + quantidade;
+    }
+}

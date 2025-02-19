2e817e2b2e340afed316c28279c016d8b082e41e
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-02-19 18:43:35 -0300

    adiciona exercicio 02 e .gitignore

---
+++ src/Aula_04/Solid_Liskov/Exercicios/Exercicio02/ContaCorrente.java
@@ -0,0 +1,7 @@
+package Aula_04.Solid_Liskov.Exercicios.Exercicio02;
+
+public class ContaCorrente extends ContaBancaria{
+    public ContaCorrente(double saldo) {
+        super(saldo);
+    }
+}
---
+++ src/Aula_04/Solid_Liskov/Exercicios/Exercicio02/ContaPoupanca.java
@@ -0,0 +1,7 @@
+package Aula_04.Solid_Liskov.Exercicios.Exercicio02;
+
+public class ContaPoupanca extends ContaBancaria{
+    public ContaPoupanca(double saldo) {
+        super(saldo);
+    }
+}
